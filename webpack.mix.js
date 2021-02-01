4577449cdf07c1b9c96f250650bbd8d9efef792f
effendikiki <raito12cs@gmail.com>
2021-02-01 10:14:37 +0700

    config frontend
    tambahan library :
    
    - postcss-nested
    - tailwindcss/typography
    - laravel-mix-purgecss
    - browser-sync
    - watch
    
    dan testing tailwind di halaman home

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,8 +12,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+mix.js('resources/js/app.js', 'public/js')
+.postCss('resources/css/app.css', 'public/css', [
     require('postcss-import'),
+    require('postcss-nested'),
     require('tailwindcss'),
     require('autoprefixer'),
-]);
+])
+.purgeCss({
+    enabled: mix.inProduction(),
+})
+.browserSync("127.0.0.1:8000");
