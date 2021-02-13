8739c8039a6881e771c8f91f7073daca7441f466
effendikiki <raito12cs@gmail.com>
2021-02-13 20:22:03 +0700

    Halaman Tag - list, add, edit

--- resources/views/admin/tag/edit.blade.php
+++ resources/views/admin/tag/edit.blade.php
@@ -1,49 +1,66 @@
-@extends('layouts.admin.app')
+@extends('layouts.admin.app-new')
 @section('content')
-<div class="row">
-    <div class="col">
+<div class="w-full p-5 mt-20 md:w-auto lg:w-4/6 xl:w-3/4">
+    <div>
         <div class="card">
             <div class="card-header">
-                <h4>Daftar Tag</h4>
+                <h6 class="h6">Update Tag</h6>
             </div>
             <div class="card-body">
                 <form action="{{route('admin.tag.update', ['tag' => $tag])}}" method="post" enctype="multipart/form-data">
                     @csrf
                     @method('PATCH')
-                    <div class="form-group">
+                    <div>
                         <label for="nama">Nama</label>
-                        <input type="text" name="nama" id="nama" class="form-control @error('nama') is-invalid @enderror" value="{{old('nama') ?? $tag->name}}">
+                        <input type="text" name="nama" id="nama" class="form-input py-2 mt-2 block w-full @error('nama') is-invalid @enderror" value="{{old('nama') ?? $tag->name}}">
                         @error('nama')
-                        <div class="alert alert-danger">
+                        <div class="mt-5 alert alert-error">
                             {{$message}}
                         </div>
                         @enderror
                     </div>
-                    <div class="form-group">
+                    <div>
                         <label for="icon">Icon</label>
-                        <div class="input-group">
-                            <span class="input-group-btn">
-                                <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary text-white">
-                                    <i class="fa fa-picture-o"></i> Choose
-                                </a>
-                            </span>
-                            <input id="thumbnail" class="form-control" type="text" name="icon" value="{{old('icon') ?? $tag->icon}}" readonly>
+                        <div class="flex items-center">
+                            <a id="lfm" data-input="thumbnail" data-preview="holder" class="pr-2 mt-2 text-white">
+                                <button id="btn_lfm" class="flex items-center align-middle btn-bs-primary">
+                                    <i class="pr-2 fad fa-camera"></i>
+                                    Pilih
+                                </button>
+                            </a>
+                            <input id="thumbnail" class="block w-full py-2 mt-2 form-input" type="text" name="icon" value="{{old('icon') ?? $tag->icon}}" readonly>
                         </div>
-                        <div id="holder" style="margin-top:15px;max-height:100px;"></div>
+                        <div class="mt-8">
+                            <label>Preview : </label>
+                            <div id="holder" class="max-w-max max-h-40" x-data>
+                                <img src="{{old('icon') ?? $tag->icon}}" class="w-full h-full border-2 border-dashed">
+                            </div>
+                        </div>
                         @error('icon')
-                        <div class="alert alert-danger">
+                        <div class="mt-5 alert alert-error">
                             {{$message}}
                         </div>
                         @enderror
                     </div>
-                    <div class=" form-group">
-                        <button type="submit" class="btn btn-primary">Update</button>
+                    <div class="flex justify-end pt-2">
+                        <button type="submit" class="w-full md:w-auto btn-bs-primary">Tambahkan</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+
+@section('customCSS')
+<style>
+    #holder img {
+        height: 10rem !important;
+        width: auto;
+        margin-top: 1rem;
+    }
+</style>
 @endsection
 
 @section('customJS')
--- resources/views/admin/tag/index.blade.php
+++ resources/views/admin/tag/index.blade.php
@@ -1,36 +1,34 @@
-@extends('layouts.admin.app')
+@extends('layouts.admin.app-new')
 @section('content')
-<div class="row">
-    <div class="col">
+<div class="w-full p-5 mt-20 md:w-auto lg:w-4/6 xl:w-3/4">
+    <div>
         <div class="card">
-            <div class="card-header">
-                <h4>Data Tag</h4>
-                <a href="{{route('admin.tag.create')}}" class="btn btn-primary">Tambah Tag</a>
+            <div class="flex justify-between card-header">
+                <h4 class="h6">Daftar Tag</h4>
+                <a href="{{route('admin.tag.create')}}" class="btn-bs-primary">Tambah Tag</a>
             </div>
-            <div class="card-body">
-                <table id="datatables" class="display" style="width:100%">
-                    <thead>
+            <div class="p-8">
+                <table id="datatables" class="w-auto py-10 text-left">
+                    <thead class="pt-10 text-white bg-gray-600 shadow-md">
                         <tr>
-                            <th>Nama Tag</th>
-                            <th>Icon</th>
-                            <th>Menu</th>
+                            <th class="px-6 pt-6 pb-4 text-sm font-bold uppercase border border-gray-600">Nama Tag</th>
+                            <th class="px-6 pt-6 pb-4 text-sm font-bold uppercase border border-gray-600">Icon</th>
+                            <th class="px-6 pt-6 pb-4 text-sm font-bold uppercase border border-gray-600">Menu</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody class="shadow-md">
                         @foreach($tags as $tag)
-                        <tr>
-                            <td>{{$tag->name}}</td>
-                            <td>
-                                <img src="{{$tag->icon}}" alt="" class="img-thumbnail">
+                        <tr class="hover:bg-gray-100">
+                            <td class="px-6 py-4">{{$tag->name}}</td>
+                            <td class="px-6 py-4">
+                                <img src="{{$tag->icon}}" alt="" class="w-auto h-20">
                             </td>
-                            <td>
-                                <a href="{{route('admin.tag.edit', ['tag' => $tag])}}" class="btn btn-success">Update</a>
+                            <td class="px-6 py-4">
+                                <a href="{{route('admin.tag.edit', ['tag' => $tag])}}" class="btn-bs-success">Update</a>
                                 <form action="{{route('admin.tag.destroy', ['tag' => $tag])}}" method="post">
                                     @csrf
                                     @method('DELETE')
-                                    <div class="form-group">
-                                        <button type="submit" class="btn btn-danger">Hapus</button>
-                                    </div>
+                                        <button type="submit" class="w-full my-2 btn-bs-danger">Hapus</button>
                                 </form>
                             </td>
                         </tr>
@@ -44,14 +42,15 @@
 @endsection
 
 @section('customCSS')
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.css" />
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.23/r-2.2.7/datatables.min.css" />
 @endsection
 
 @section('customJS')
-<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.23/r-2.2.7/datatables.min.js"></script>
 <script>
     $(document).ready(function() {
         $('#datatables').DataTable({
+            responsive: true,
             "language": {
                 "url": "https://cdn.datatables.net/plug-ins/1.10.22/i18n/Indonesian.json"
             }
