7c9eefc60037b87b4381f892268f5de2e5361f0c
effendikiki <raito12cs@gmail.com>
2021-02-22 17:05:51 +0700

    update Component - Card Course (see details)
    - add dropdown menu detail (show)
    - add dropdown menu episode
    - add missing image alternative
    - change icon total murid -> total episode
    - add link total episode

--- resources/views/components/card-course.blade.php
+++ resources/views/components/card-course.blade.php
@@ -1,6 +1,7 @@
 <!-- start card -->
-<div class="w-full px-2 py-4 mx-auto report-card md:w-1/2 2xl:w-1/3">
+<div class="w-full px-2 py-4 report-card md:w-1/2 2xl:w-1/3">
     <div class="h-full px-5 pt-8 pb-5 bg-white border border-gray-200 rounded-md shadow card ">
+        <!-- card -->
         <div class="flex flex-col">
             <!-- bagian atas -->
             <div class="flex justify-end text-gray-400 " x-data="{detailOpen : false}">
@@ -12,94 +13,120 @@
 
                 <div x-cloak x-show.transition.origin.top="detailOpen" @click.away="detailOpen = false" class="absolute z-50 w-40 py-2 mt-5 ml-10 text-left text-gray-500 bg-white border border-gray-300 rounded shadow-md">
                     <!-- item -->
-                    <!-- <a href="#" class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="#">
+                    @role('admin')
+                    <a href="{{route('admin.course.show', $courseId)}}" class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900">
                         <i class="mr-1 text-xs fas fa-info"></i>
                         Detail
-                    </a> -->
+                    </a>
+                    @else
+                    <a href="{{route('mentor.course.show', $courseId)}}" class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900">
+                        <i class="mr-1 text-xs fas fa-info"></i>
+                        Detail
+                    </a>
+                    @endrole
                     <!-- end item -->
 
                     <!-- item -->
                     @role('admin')
                     <a href="{{route('admin.course.edit', $courseId)}}" class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="#">
-                        @else
-                        <a href="{{route('mentor.course.edit', $courseId)}}" class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="#">
-                            @endrole
+                        <i class="mr-1 text-xs fas fa-edit"></i>
+                        Edit
+                    </a>
+                    @else
+                    <a href="{{route('mentor.course.edit', $courseId)}}" class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="#">
+                        <i class="mr-1 text-xs fas fa-edit"></i>
+                        Edit
+                    </a>
+                    @endrole
+                    <!-- end item -->
 
-                            <i class="mr-1 text-xs fas fa-edit"></i>
-                            Edit
-                        </a>
-                        <!-- end item -->
+                    <!-- item -->
+                    {{-- @role('admin')
+                    <a href="{{route('admin.course.episode', $courseId)}}" class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="#">
+                    <i class="mr-1 text-xs fas fa-photo-video"></i>
+                    Episode
+                    </a>
+                    @else
+                    <a href="{{route('mentor.course.episode', $courseId)}}" class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="#">
+                        <i class="mr-1 text-xs fas fa-photo-video"></i>
+                        Episode
+                    </a>
+                    @endrole --}}
+                    <!-- end item -->
 
-                        <!-- item -->
-
-                        <!-- form di hidden -->
-                        <div x-data>
-                            @role('admin')
-                            <form action="{{route('admin.course.destroy', $courseId)}}" method="post" class="hidden">
-                                @else
+                    <!-- item -->
+                    <!-- form di hidden -->
+                    <div x-data>
+                        @role('admin')
+                        <form action="{{route('admin.course.destroy', $courseId)}}" method="post" class="hidden">
+                            @else
                             <form action="{{route('mentor.course.destroy', $courseId)}}" method="post" class="hidden">
                                 @endrole
-                                    @csrf
-                                    @method('DELETE')
-                                    <button id="{{ $title }}" type="submit">
-                                        Hapus
-                                    </button>
-
-                                </form>
-
-                                <a href="#" @click.prevent="$('#{{ $title }}').click();" class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900">
-                                    <i class="mr-1 text-xs fas fa-trash"></i>
+                                @csrf
+                                @method('DELETE')
+                                <button id="{{ $title }}" type="submit">
                                     Hapus
-                                </a>
-                        </div>
-                        <!-- end item -->
+                                </button>
+
+                            </form>
+
+                            <a href="#" @click.prevent="$('#{{ $title }}').click();" class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900">
+                                <i class="mr-1 text-xs fas fa-trash"></i>
+                                Hapus
+                            </a>
+                    </div>
+                    <!-- end item -->
                 </div>
 
             </div>
             <div class="flex flex-col items-center md:flex-row md:items-start">
+
+                @if($thumbnail)
                 <img src=" {{$thumbnail}} " alt="missing img" class="object-cover w-20 h-20 rounded-full" />
-
-                <!-- jika image kosong pake gambar dibawah -->
-
-                <!-- <img src="https://media.istockphoto.com/vectors/no-image-vector-symbol-missing-available-icon-no-gallery-for-this-vector-id1128826884?k=6&m=1128826884&s=170667a&w=0&h=F6kUwTcsLXUojmGFxN2wApEKgjx63zcIshCSOmnfEFs=" alt="missing img" class="object-cover w-20 h-20 rounded-full" /> -->
+                @else
+                <img src="https://i.stack.imgur.com/y9DpT.jpg" alt="missing img" class="object-cover w-20 h-20 border rounded-full" />
+                @endif
 
                 <div class="flex flex-col items-start pl-5">
                     <h3 class="text-xl font-semibold hover:text-blue-600">
                         <a href="{{route('admin.course.edit', $courseId)}}">{{ $title }}</a>
                     </h3>
-                    <!-- kalo bisa mending get tanggal dibuat -->
-                    <!-- <span class="text-sm tracking-tight text-gray-500">Dibuat pada : 12 Oktober 2020</span> -->
+
                     <span class="text-sm tracking-tight text-gray-500"> {{ $level }} </span>
                     <div class="flex flex-wrap gap-1 mt-1">
-                        <!-- bikin perulangan disini -->
                         @foreach($tags as $tag)
                         <span class="px-2 py-1 text-xs tracking-tight text-gray-100 bg-blue-400 rounded-full"> {{ $tag->name }} </span>
                         @endforeach
-                        <!-- bikin perulangan disini -->
                     </div>
                 </div>
             </div>
             <!-- end of bagian atas -->
 
             <!-- bagian bawah -->
-            <div class="flex pt-5 text-center">
+            <div class="flex pt-5">
+
                 <!-- kiri -->
-                <div class="flex items-center justify-center flex-1 gap-1 px-1 text-gray-700 border-r-2">
-                    <svg class="w-14 h-14" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path>
-                    </svg>
+
+                <div class="flex items-center justify-center flex-1 gap-1 px-1 text-gray-700 border-r-2 md:-ml-6 hover:text-blue-600">
+                    <!-- link masih disabled -->
+                    <a href="{{-- route('admin.course.episode', $courseId) --}}">
+                        <div class="mr-2 ">
+                            <i class="text-4xl md:text-5xl fab fa-youtube"></i>
+                        </div>
+                    </a>
                     <div class="flex flex-col items-start">
                         <h3 class="text-xl font-bold md:text-2xl xl:text-3xl">31</h3>
-                        <span class="text-xs tracking-tight text-gray-500 md:text-sm"><span class="hidden sm:inline">Total</span> Murid</span>
+                        <span class="text-xs tracking-tight text-gray-500 md:text-sm"><span class="hidden sm:inline">Total</span> Episode</span>
                     </div>
+
                 </div>
+
 
                 <!-- kanan -->
                 <div class="flex items-center justify-center flex-1 gap-1 px-1 text-gray-700">
-                    <svg class="w-14 h-14" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-                        <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm9.707 5.707a1 1 0 00-1.414-1.414L9 12.586l-1.293-1.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
-                    </svg>
+                    <div class="mr-2 ">
+                        <i class="text-4xl md:text-5xl fas fa-clipboard"></i>
+                    </div>
                     <div class="flex flex-col items-start">
                         <h3 class="text-xl font-bold md:text-2xl xl:text-3xl">11</h3>
                         <span class="text-xs tracking-tight text-gray-500 md:text-sm"> Submission</span>
@@ -109,6 +136,7 @@
             </div>
             <!-- end of bagian bawah -->
         </div>
+        <!-- end of card -->
     </div>
 </div>
 <!-- end card -->
