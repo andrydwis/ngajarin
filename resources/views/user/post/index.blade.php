2ce483c27e81e3536a6999d0eda01ab5d3f1204f
effendikiki <43091431+effendikiki@users.noreply.github.com>
2021-04-13 11:31:50 +0700

    styling post - index

--- resources/views/user/post/index.blade.php
+++ resources/views/user/post/index.blade.php
@@ -1,28 +1,260 @@
-<h1> post</h1>
-<a href="{{route('user.post.create')}}">tambah baru</a>
-<hr>
+@extends('layouts.student.app-new')
+@section('content')
 
-@foreach($posts as $post)
-@if($post->creator->id == auth()->user()->id)
-<a href="{{route('user.post.edit', ['post' => $post->slug])}}">Edit postingan</a>
-<form action="{{route('user.post.destroy', ['post' => $post->slug])}}" method="post">
-@csrf
-@method('DELETE')
-<button type="submit">Hapus post</button>
-</form>
-@endif
-<br>
-tag :
-@foreach($post->tags as $tag)
-{{$tag->name}}
-@endforeach <br>
-judul : {{$post->title}} <br>
-konten : {{Str::words($post->content, 5, '...')}} <br>
-dibuat oleh : {{$post->creator->name}} <br>
-dibuat tanggal: {{\Carbon\Carbon::parse($post->created_at)->isoFormat('dddd, D MMMM Y')}}, {{$post->created_at->diffForHumans()}} <br>
-Jumlah komen : {{$post->comments->count()}} <br>
-Jumlah like : {{$post->reacts->where('type', 'like')->count()}} <br>
-Jumlah dislike : {{$post->reacts->where('type', 'dislike')->count()}} <br>
-link : <a href="{{route('user.post.show', ['post' => $post])}}">lihat</a>
-<hr>
-@endforeach
+<div class="flex justify-center">
+
+    <div class="flex flex-col items-start max-w-5xl py-5 md:flex-row md:mx-5 ">
+
+        <div class="flex-none hidden mr-9 lg:block lg:sticky lg:self-start top-10 w-52">
+
+            <div class="lg:sticky lg:text-center">
+
+                <!-- isi sidebar -->
+                <div class="flex flex-col pt-10">
+
+                    <a href="{{route('user.post.create')}}">
+                        <div class="pl-6 my-1 text-base text-left text-white sidebar-item bg-primary-lighter hover:bg-primary hover:text-white">
+                            <i class="mr-1 text-sm text-white fas fa-plus"></i> Postingan Baru
+                        </div>
+                    </a>
+
+                    <hr class="my-4 ml-2">
+
+                    <a href="">
+                        <div class="pl-6 my-1 text-base text-left border border-white sidebar-item hover:border-primary sidebar-item-active">
+                            <i class="mr-1 text-sm fill-current fas fa-bars"></i> Postingan Terbaru
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="pl-6 my-1 text-base text-left border border-white sidebar-item hover:border-primary">
+                            <i class="mr-1 text-sm fill-current fas fa-star"></i> Upvote Tertinggi
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="pl-6 my-1 text-base text-left border border-white sidebar-item hover:border-primary">
+                            <i class="mx-1 text-sm fill-current fas fa-question"></i> Belum terpecahkan
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="pl-6 my-1 text-base text-left border border-white sidebar-item hover:border-primary">
+                            <i class="mx-1 text-sm fill-current fas fa-history"></i> Postingan Saya
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="pl-6 my-1 text-base text-left border border-white sidebar-item hover:border-primary">
+                            <i class="ml-1 mr-2 text-sm fill-current fas fa-bookmark"></i> Disimpan
+                        </div>
+                    </a>
+
+                </div>
+                <!-- end of sidebar -->
+
+
+            </div>
+
+        </div>
+
+        <div class="flex-1">
+
+            <!-- search section -->
+            <form action="#" autocomplete="off" class="flex justify-end px-6 mb-8 pt-11">
+
+                <select class="pl-5 mr-3 text-sm text-gray-500 rounded-full cursor-pointer pr-9 form-select">
+                    <option value="all" selected>Semua Topik</option>
+                    <option value="kuliah">Kuliah</option>
+                    <option value="javascript">JavaScript</option>
+                    <option value="laravel">Laravel</option>
+                    <option value="livewire">Livewire</option>
+                    <option value="tailwind">Tailwind</option>
+                    <option value="alpine">Alpine.js</option>
+                </select>
+
+
+                <div class="flex w-full md:w-72">
+                    <input name="search" placeholder="Mau cari apa?" value="" class="w-full pl-5 pr-10 text-sm placeholder-gray-500 rounded-full form-input">
+                    <button type="submit" class="-ml-8 text-gray-400 focus:outline-none hover:text-primary focus:text-primary-darker">
+                        <i class="text-sm fill-current fas fa-search"></i>
+                    </button>
+                </div>
+
+            </form>
+            <!-- end of search section -->
+
+
+            <!-- postingan list -->
+            <div class="py-3">
+                @foreach($posts as $post)
+
+                <div class="flex flex-col h-auto px-6 py-4 mx-4 mb-4 transition-all bg-gray-100 border border-gray-100 cursor-pointer lg:h-36 md:flex-row bg-opacity-30 md:hover:bg-gray-100 rounded-xl">
+
+                    <div class="flex items-center self-start w-full mb-4 md:w-auto md:mr-5 md:block md:mb-0">
+
+                        <!-- user info -->
+                        <div class="flex items-center">
+                            <a href="/@eludic" class="flex mb-2 mr-3 md:mr-0">
+                                <img loading="lazy" alt="eludic" class="relative object-cover bg-white rounded-lg h-14 w-14" src="{{url('/img/user1.jpg')}}" />
+                            </a>
+                            <strong class="text-xs uppercase md:text-base md:hidden">
+                                {{$post->creator->name}}
+                            </strong>
+                        </div>
+                        <!-- end of user info -->
+
+                        <!-- jumlah komen & tag -->
+                        <div class="flex ml-auto">
+
+                            <div class="flex items-center justify-center py-1 ml-auto md:hidden">
+                                <div class="flex items-center px-3 py-2 text-gray-400 bg-gray-100 rounded-full">
+                                    <i class="mr-2 text-xs fas fa-comment"></i>
+                                    <span class="text-xs font-semibold leading-none">
+                                        {{$post->comments->count()}}
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div class="md:hidden">
+                                <a href="https://laracasts.com/discuss/channels/laravel" class="block py-1 text-xs text-center rounded-full btn btn-outline-danger"> Laravel </a>
+                            </div>
+
+                        </div>
+                        <!-- end of jumlah komen & tag -->
+
+                    </div>
+
+                    <div class="w-full md:mb-0">
+                        <div class="flex flex-col justify-between h-full">
+
+                            <div class="md:flex md:items-start lg:-mt-1">
+
+                                <!-- title -->
+                                <h4 class="mb-4 text-base break-words lg:mb-0 md:pr-6 lg:line-clamp-1">
+                                    <a href="{{route('user.post.show', ['post' => $post])}}" class="text-lg font-semibold text-black conversation-list-link hover:text-black link" title="Redirect on Session expire"> {{$post->title}} </a>
+                                </h4>
+                                <!-- end of title -->
+
+                                <!-- comment, view, tag -->
+                                <div class="relative hidden text-center text-gray-400 md:flex md:items-center md:flex-row-reverse md:ml-auto">
+
+                                    <div class="flex ml-5">
+
+                                        <!-- <div>
+                                                @foreach($post->tags as $tag)
+                                                <a href="#" class="block py-1 mx-0 my-0 ml-1 text-center rounded-full btn btn-outline-danger"> {{$tag->name}}
+                                                </a>
+                                                @endforeach
+                                            </div> -->
+
+                                        <a href="#" class="block py-1 mx-0 my-0 ml-1 text-center rounded-full btn btn-outline-danger"> Laravel </a>
+
+                                    </div>
+                                    <div class="flex items-center justify-center ml-4">
+                                        <i class="mr-1 text-sm fas fa-thumbs-up"></i>
+                                        <span class="relative text-xs font-semibold leading-none text-left">
+                                            {{$post->reacts->where('type', 'like')->count()}}
+                                        </span>
+                                    </div>
+                                    <div class="flex items-center justify-center">
+                                        <i class="mr-1 text-sm fas fa-comment"></i>
+                                        <span class="relative text-xs font-semibold leading-none text-left">
+                                            {{$post->comments->count()}}
+                                        </span>
+                                    </div>
+                                </div>
+                                <!-- end of comment, view, tag -->
+
+                            </div>
+
+                            <!-- potongan konten -->
+                            <div class="mt-2 mb-6 text-gray-800 lg:mb-0 lg:pr-8">
+                                <a href="{{route('user.post.show', ['post' => $post])}}">
+                                    <span class="normal-case break-words line-clamp-none lg:line-clamp-2 ">
+                                        {{$post->content}}
+                                    </span>
+                                </a>
+                            </div>
+                            <!-- end of potongan konten -->
+
+                            <!-- last reply info -->
+                            <div class="flex flex-col justify-between mt-4 text-xs text-gray-700 md:flex-row">
+                                <div>
+
+                                    Diposting
+
+                                    <a href="#">
+                                        <time class="font-bold ">
+                                            {{$post->created_at->diffForHumans()}}
+                                        </time>
+                                    </a>
+                                </div>
+
+                                <div class="flex gap-2 mt-2 text-sm md:mt-0">
+                                    @if($post->creator->id == auth()->user()->id)
+                                    <a href="{{route('user.post.edit', ['post' => $post->slug])}}" class="pr-2 font-semibold border-r-2 hover:text-primary">Edit</a>
+
+                                    <form action="{{route('user.post.destroy', ['post' => $post->slug])}}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="font-semibold hover:text-primary">Hapus</button>
+                                    </form>
+                                    @endif
+                                </div>
+
+                            </div>
+                            <!-- end of last reply info -->
+
+
+                        </div>
+                    </div>
+                </div>
+
+                @endforeach
+            </div>
+            <!-- end of postingan list -->
+
+
+
+            <!-- WARNING : OJO DIHAPUS!! -->
+            <!-- <div>
+                <h1> post</h1>
+                <a href="{{route('user.post.create')}}">tambah baru</a>
+                <hr>
+
+                @foreach($posts as $post)
+
+                @if($post->creator->id == auth()->user()->id)
+                <a href="{{route('user.post.edit', ['post' => $post->slug])}}">Edit postingan</a>
+                <form action="{{route('user.post.destroy', ['post' => $post->slug])}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Hapus post</button>
+                </form>
+                @endif
+
+                <br>
+                tag :
+                @foreach($post->tags as $tag)
+                {{$tag->name}}
+                @endforeach <br>
+                judul : {{$post->title}} <br>
+                konten : {{Str::words($post->content, 5, '...')}} <br>
+                dibuat oleh : {{$post->creator->name}} <br>
+                dibuat tanggal: {{\Carbon\Carbon::parse($post->created_at)->isoFormat('dddd, D MMMM Y')}}, {{$post->created_at->diffForHumans()}} <br>
+                Jumlah komen : {{$post->comments->count()}} <br>
+                Jumlah like : {{$post->reacts->where('type', 'like')->count()}} <br>
+                Jumlah dislike : {{$post->reacts->where('type', 'dislike')->count()}} <br>
+                link : <a href="{{route('user.post.show', ['post' => $post])}}">lihat</a>
+                <hr>
+                @endforeach
+            </div> -->
+            <!-- WARNING : OJO DIHAPUS!! -->
+
+        </div>
+    </div>
+
+
+
+    @endsection
