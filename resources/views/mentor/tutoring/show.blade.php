5dafa2591782e5bea72353cbf3e0146d2d8580d1
effendikiki <43091431+effendikiki@users.noreply.github.com>
2021-04-26 09:10:59 +0700

    minor fix
    - layouting create dan status request

--- resources/views/mentor/tutoring/show.blade.php
+++ resources/views/mentor/tutoring/show.blade.php
@@ -13,69 +13,90 @@
         </div>
 
         <!-- card body -->
-        <div class="py-4 prose card-body ">
+        <div class="py-4 card-body">
 
-            <div>
-                <label>Nama </label>
-                <div class="w-full cursor-not-allowed md:w-1/2 form-input">
-                    {{$tutoring->student->name}}
-                </div>
-            </div>
+            <div class="prose">
 
-            <div class="flex flex-wrap items-center gap-2 mt-5">
-                <div class="w-full md:flex-1">
-                    <label>Tanggal</label>
-                    <div class="w-full cursor-not-allowed form-input">
-                        {{\Carbon\Carbon::parse($tutoring->date)->isoFormat('dddd, D MMMM Y')}}
+                <div>
+                    <label>Nama </label>
+                    <div class="w-full cursor-not-allowed md:w-1/2 form-input">
+                        {{$tutoring->student->name}}
                     </div>
                 </div>
 
-                <div class="flex flex-wrap w-full gap-2 xs:flex-nowrap md:flex-1">
-                    <div class="w-1/2">
-                        <label for="jam_mulai">jam mulai</label>
-                        <div class="block w-full cursor-not-allowed form-input">
-                            {{$tutoring->hour_start}}
+                <div class="flex flex-wrap items-center gap-2 mt-5">
+                    <div class="w-full md:flex-1">
+                        <label>Tanggal</label>
+                        <div class="w-full cursor-not-allowed form-input">
+                            {{\Carbon\Carbon::parse($tutoring->date)->isoFormat('dddd, D MMMM Y')}}
                         </div>
                     </div>
 
-                    <div class="w-1/2">
-                        <label for="jam_akhir">jam akhir</label>
-                        <div class="block w-full cursor-not-allowed form-input">
-                            {{$tutoring->hour_end}}
+                    <div class="flex flex-wrap w-full gap-2 xs:flex-nowrap md:flex-1">
+                        <div class="w-1/2">
+                            <label for="jam_mulai">jam mulai</label>
+                            <div class="block w-full cursor-not-allowed form-input">
+                                {{$tutoring->hour_start}}
+                            </div>
                         </div>
+
+                        <div class="w-1/2">
+                            <label for="jam_akhir">jam akhir</label>
+                            <div class="block w-full cursor-not-allowed form-input">
+                                {{$tutoring->hour_end}}
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="w-full mt-5">
+                    <label for="detail">Detail</label>
+                    <textarea name="detail" class="block w-full form-textarea" rows="3" disabled> {{$tutoring->detail}} </textarea>
+                </div>
+
+                <div class="mt-5">
+                    <label>Status </label>
+                    <div class="w-full cursor-not-allowed md:w-1/3 form-input">
+                        {{$tutoring->status}}
                     </div>
                 </div>
 
-            </div>
-
-            <div class="w-full mt-5">
-                <label for="detail">Detail</label>
-                <textarea name="detail" class="block w-full form-textarea" rows="3" disabled> {{$tutoring->detail}} </textarea>
-            </div>
-
-            <div class="mt-5">
-                <label>Status </label>
-                <div class="w-full cursor-not-allowed md:w-1/3 form-input">
-                    {{$tutoring->status}}
-                </div>
             </div>
 
             <hr class="my-10">
 
-            <div class="flex justify-end">
+            <div class="flex flex-col justify-between prose md:flex-row">
+                <div>
+                    <a href="{{route('mentor.tutoring.index')}}">
+                        <button type="button" class="w-full px-10 mt-5 text-base border-none md:w-auto md:mt-0 btn btn-outline-primary hover:border">Kembali</button>
+                    </a>
+                </div>
 
-                @if($tutoring->status == 'menunggu')
-                <form action="{{route('mentor.tutoring.update', ['tutoring' => $tutoring])}}" method="POST">
-                    @csrf
-                    @method('PATCH')
-                    <select name="status" class="form-select">
-                        <option value="">Pilih Aksi</option>
-                        <option value="diterima">terima</option>
-                        <option value="ditolak">tolak</option>
-                    </select>
-                    <button type="submit" class="px-20 mt-5 text-base md:mt-0 btn btn-primary">proses</button>
-                </form>
-                @endif
+                <div class="flex flex-col md:flex-row">
+                    @if($tutoring->status == 'menunggu')
+                    <form action="{{route('mentor.tutoring.update', ['tutoring' => $tutoring])}}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <select name="status" class="form-select" hidden>
+                            <option selected value="ditolak">ditolak</option>
+                        </select>
+                        <button type="submit" class="w-full px-10 mt-2 text-base md:w-auto md:mt-0 btn btn-outline-primary">Tolak</button>
+                    </form>
+                    @endif
+
+                    @if($tutoring->status == 'menunggu')
+                    <form action="{{route('mentor.tutoring.update', ['tutoring' => $tutoring])}}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <select name="status" class="form-select" hidden>
+                            <option selected value="diterima">diterima</option>
+                        </select>
+                        <button type="submit" class="w-full px-10 mt-2 text-base md:w-auto md:mt-0 btn btn-primary">Terima</button>
+                    </form>
+                    @endif
+                </div>
+
             </div>
 
         </div>
